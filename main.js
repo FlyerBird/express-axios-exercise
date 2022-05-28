3080f56d7c69e652e1c23ab7f65e5367a66be047
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-05-28 13:11:00 +0200

    done

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 const express = require('express');
 const axios = require('axios');
+const { response } = require('express');
 
 const app = express();
 
 // Iteration 1
 app.get('/pikachu', (req, res, next) => {
-  // https://pokeapi.co/api/v2/pokemon/pikachu
-
+  axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then((response) => {
+      res.json(response.data)
+    })
+  .catch(err => console.log(err));
 })
 
 // Iteration 2
-app.get('/rockets', (req, res, next) => {
-  // https://api.spacexdata.com/v4/rockets
-
+app.get('/rockets', async (req, res, next) => {
+  app.get('/randomuser', async (req, res, next) => {
+    try {
+      const response = await axios.get('https://api.spacexdata.com/v4/rockets'); 
+      res.json(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }) 
 })
 
 // Iteration 3
-app.get('/randomuser', (req, res, next) => {
-  // https://randomuser.me/api/
-
+app.get('/randomuser', async (req, res, next) => {
+  try {
+    const response = await axios.get('https://https://randomuser.me/api/'); 
+    res.json(response.data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 // Iteration 4
-// app.get...
+app.get('/', (req, res, next) => {
+
+res.send(`<a href= "/pikachu">pikachu</a>`)
+res.send(`<a href= "/rockets">rockets</a>`)
+res.send(`<a href= "/randomuser">randomuser</a>`)
+
+})
 
 app.listen(3000, () => { console.log('Server running on port 3000 🛫') })
